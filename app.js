d2b6d2e632a7a39816afc22986949ba9d51ecbf2
John Allen Blaxton <ja.blaxton@gmail.com>
2024-08-28 16:21:12 -0500

    new files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path'); // Make sure to require 'path'
+const app = express();
 
 console.log("I'm on a node server");
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.get('/', function (req, res) {
-  res.send('Hello node from Ex on this here local dev box')
-})
-
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
