e96284da3c56d2542e527a0dcc2e85bd4e65b38d
John Allen Blaxton <ja.blaxton@gmail.com>
2024-08-28 16:30:02 -0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const path = require('path'); // Make sure to require 'path'
+const path = require('path');
 const app = express();
 
-console.log("I'm on a node server");
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  
 });
