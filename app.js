c0aa078fefebebddff98dcce541e5c3ff5426830
John Allen Blaxton <ja.blaxton@gmail.com>
2024-09-18 23:20:31 -0500

    IT WORKS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const uri = process.env.MONGO_URI;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-app.use(express.static('./public/'))
+app.use(express.static('./'))
 
 console.log(uri);
 
