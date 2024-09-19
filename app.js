49615c83d900df00898fa5984280053e30a20856
John Allen Blaxton <ja.blaxton@gmail.com>
2024-09-18 21:41:01 -0500

    Updates

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 require('dotenv').config()
-const express = require('express');
-const path = require('path');
-const app = express();
-app.set('view engine', 'ejs')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-//mongo code
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+console.log(uri);
+console.log('Server is running with MongoDB connection');
+
+//client
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,36 +25,73 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/mongo', async (req,res)=>{
-  await client.connect();
-  let result = await client.db("jab-db").collection("dev-john(allen)")
-   .find({}).toArray();
-  console.log(result);
-
-  res.render('index', {
-    mongoResult : result
-  });
-
+//get index
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
 })
 
+// Serve EJS
+app.get('/ejs', (req, res) => {
+  res.render('index', {
+    myServerVariable: "something from server"
+  });
+})
 
-
-app.use(express.static(('./')));
-
-
-app.listen(3000, () => {
+// read data
+app.get('/read', async (req, res) => {
+  await client.connect();
+  let result = await client.db("jab-db").collection("dev-john(allen)").find({}).toArray();
+  console.log(result);
   
-});
+  res.render('mongo', {
+    postData: result
+  });
+})
 
+// hardcode mongodb
+app.get('/insert', async (req, res) => {
+  await client.connect();
+  await client.db("jab-db").collection("dev-john(allen)").insertOne({ post: 'hardcoded post insert' });
+  await client.db("jab-db").collection("dev-john(allen)").insertOne({ iJustMadeThisUp: 'hardcoded new key' });
+  
+  res.render('insert');
+})
+
+// Update mongodb
+app.post('/update/:id', async (req, res) => {
+  await client.connect();
+  const collection = client.db("jab-db").collection("dev-john(allen)");
+  let result = await collection.findOneAndUpdate(
+    { "_id": new ObjectId(req.params.id) },
+    { $set: { "post": "NEW POST" } }
+  );
+  
+  console.log(result);
+  res.redirect('/read');
+})
+
+// Delete on mongodb
+app.post('/delete/:id', async (req, res) => {
+  await client.connect();
+  const collection = client.db("jab-db").collection("dev-john(allen)");
+  let result = await collection.findOneAndDelete(
+    { "_id": new ObjectId(req.params.id) }
+  );
+  
+  console.log(result);
+  res.redirect('/read');
+})
+
+// server
+app.listen(5500, () => {
+  console.log('Server is on port 5500');
+});
