4dc9fab6d7456bf1f8896809ea6aa430ad7a37e8
JohnAllenB <ja.blaxton@gmail.com>
2024-10-03 12:44:46 -0500

    Add PORT

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { urlencoded } = require('body-parser')
 const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -30,72 +31,75 @@ app.get('/', function (req, res) {
   res.sendFile('index.html');
 })
 
-app.get('/ejs', (req,res)=>{
-``
+app.get('/ejs', (req, res) => {
+  ``
   res.render('index', {
-    myServerVariable : "something from server"
+    myServerVariable: "something from server"
   });
 })
 
-app.get('/read', async (req,res)=>{
+app.get('/read', async (req, res) => {
 
   console.log('in /read');
   await client.connect();
-  
+
   console.log('connected?');
   //ping to confirm 
-  
+
   let result = await client.db("jab-db").collection("dev-john(allen)")
-    .find({}).toArray(); 
-  console.log(result); 
+    .find({}).toArray();
+  console.log(result);
 
   res.render('read', {
-    postData : result
+    postData: result
   });
 
 })
 
-app.get('/insert', async (req,res)=> {
+app.get('/insert', async (req, res) => {
 
   console.log('in /insert');
   await client.connect();
-  await client.db("jab-db").collection("dev-john(allen)").insertOne({ post: 'hardcoded post insert '});
-  await client.db("jab-db").collection("dev-john(allen)").insertOne({ iJustMadeThisUp: 'hardcoded new key '});  
+  await client.db("jab-db").collection("dev-john(allen)").insertOne({ post: 'hardcoded post insert ' });
+  await client.db("jab-db").collection("dev-john(allen)").insertOne({ iJustMadeThisUp: 'hardcoded new key ' });
   res.render('insert');
-
-}); 
-
-app.post('/update/:id', async (req,res)=>{
-
-  console.log("req.parms.id: ", req.params.id)
-
-  client.connect; 
-  const collection = client.db("jab-db").collection("dev-john(allen)");
-  let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "Roll Tide" } }
-)
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
-})
 
 });
 
-app.post('/delete/:id', async (req,res)=>{
+app.post('/update/:id', async (req, res) => {
 
   console.log("req.parms.id: ", req.params.id)
 
-  client.connect; 
+  client.connect;
   const collection = client.db("jab-db").collection("dev-john(allen)");
-  let result = await collection.findOneAndDelete( 
-  {"_id": new ObjectId(req.params.id)})
+  let result = await collection.findOneAndUpdate(
+    { "_id": new ObjectId(req.params.id) }, { $set: { "post": "Roll Tide" } }
+  )
+    .then(result => {
+      console.log(result);
+      res.redirect('/read');
+    })
 
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
+});
+
+app.post('/delete/:id', async (req, res) => {
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect;
+  const collection = client.db("jab-db").collection("dev-john(allen)");
+  let result = await collection.findOneAndDelete(
+    { "_id": new ObjectId(req.params.id) })
+
+    .then(result => {
+      console.log(result);
+      res.redirect('/read');
+    })
+
+  //port 5500
+
 })
 
-//port 5500
-
-})
-app.listen(5500)
+app.listen(PORT, () => {
+  console.log(`Server is running & listening on port ${PORT}`);
+});
